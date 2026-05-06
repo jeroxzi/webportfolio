e8d3dffda518c9f1c26e9ceb36804d3ea6705f41
compjerox <compprogjv@gmail.com>
2026-05-06 22:58:36 +0800

    feat: update to new premium portfolio

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/JeroxziPortfolio' : '',
+  basePath: isProd ? '/webportfolio' : '',
   images: {
     unoptimized: true,
   },
